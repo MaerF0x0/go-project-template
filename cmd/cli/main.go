24079d61d2d5d2e301aa8d57c574e799a4905cf0
Michael Graf <mikegraf000@gmail.com>
2024-02-01 22:11:41 -0600

    Demonstrate using build flags to set version number into the logs
    `go build -ldflags="-X 'main.Version=0.0.0.rc123'"`

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -6,9 +6,20 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+var (
+	// set version at build time with
+	// go build -ldflags="-X 'main.Version=0.0.0.rc123'"
+	Version        string
+	CommitHash     = "n/a"
+	BuildTimestamp = "n/a"
+)
+
 func main() {
 	log.SetFormatter(&log.JSONFormatter{})
+
 	log.WithFields(log.Fields{
-		"Args": os.Args,
+		"Args":       os.Args,
+		"Version":    Version,
+		"CommitHash": CommitHash,
 	}).Info("Example CLI")
 }
